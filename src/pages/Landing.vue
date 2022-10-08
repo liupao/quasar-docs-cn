f1b50951b0a7fd03370c081b3717b3254b57c549
dongwa <783137663@qq.com>
2022-10-09 07:21:07 +0800

    chore: 优化起始页

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,134 +1,156 @@
-<template lang="pug">
-q-page.landing
-  h1.landing__title Quasar Framework 中文网站
-  section.landing__hero.flex.flex-center
-    .row.items-center.justify-center.landing__hero-inner
-      .landing__hero-logo.col-12.flex.flex-center.q-mb-lg
-        img.landing__logo-image(src="https://cdn.quasar.dev/logo-v2/svg/logo-vertical.svg")
-      .landing__hero-text.col-12.column.items-center
-        //- .text-h4.landing__hero-text-main.text-bold.text-brand-primary.q-pb-xs QUASAR FRAMEWORK
-        .q-pb-lg.text-grey-6.landing__hero-quote.text-center
-          | 帮助您在短时间内快速构建高性能，高质量的
-          | <span class="text-bold text-no-wrap">Vue.js 3</span> 应用程序
-        .landing__hero-row.q-gutter-sm.row.items-center
-          q-btn(color="brand-primary" unelevated no-caps to="/introduction-to-quasar" label="为何选择Quasar?")
-          q-btn(color="brand-primary" outline no-caps to="/start" label="Get Started")
-          q-btn(color="accent" unelevated no-caps to="/video-tutorials" label="视频教程")
-        .q-pb-lg.text-grey-6.landing__hero-quote.text-center
-          | 本中文网站持续翻译中,信息来自：<a target="_blank" class="text-bold text-no-wrap" href="https://quasar.dev">英文官网</a>。
-          | 如果想了解翻译进度，或者参与翻译，请访问 <a target="_blank" class="text-bold text-no-wrap" href="https://github.com/dongwa/quasar-docs-cn">仓库地址</a>。
-          | 另外，我的B站账号中有<a target="_blank" class="text-bold text-no-wrap" href="https://www.bilibili.com/video/BV1pA4y197Zc">quasar的视频教程</a>欢迎观看👏
-  section.landing__front.text-center
-    div
-      .row.justify-center
-        introduction-video.landing__video
-
-  section.bg-white.text-grey-10.text-center
-    div
-      .text-h4.q-mb-xl 我们的高级赞助商
-      sponsor-list
-
-  section.bg-white.text-grey-10.text-center.row.justify-center
-    .landing__features.row
-      .col-12.text-center
-        h4 一套代码支持全平台
-      .col-12.text-center.q-my-xl
-        .landing__features-icons.q-gutter-lg
-          q-icon(:name="fabApple")
-          q-icon(:name="fabWindows")
-          q-icon(:name="fabLinux")
-          q-icon(:name="fabAndroid")
-          q-icon(:name="fabChrome")
-          q-icon(:name="fabFirefox")
-          q-icon(:name="fabEdge")
-          q-icon(:name="fabSafari")
-      .col-12.text-center.q-my-xl
-        p 使用官网脚手架@quasar/cli创建的quasar项目可以一套源代码编译成所有平台的应用程序，并提供最新和最好的最佳实践。你只需关注你的应用功能本身，忘记跨端带来的烦恼。
-      .col-12.row.justify-center
-        q-card.bg-grey-1.q-px-lg.q-py-sm(bordered flat)
-          q-card-section(horizontal)
-            q-card-section.text-left.column.items-center.q-gutter-sm
-              q-badge(label="$ yarn global add @quasar/cli" color="grey-8")
-              q-badge(label="$ yarn create quasar" color="grey-8")
-            q-separator(vertical)
-            q-card-section.text-left.column.items-center.q-gutter-sm
-              q-badge(label="$ npm i -g @quasar/cli" color="grey-8")
-              q-badge(label="$ npm init quasar" color="grey-8")
-
-  section.bg-white.text-grey-10.text-center
-    .landing__features.row.items-start.q-col-gutter-lg
-      .col-12.text-center
-        h4 Impressive User Interface Building Blocks
-      .col-12.row
-        .col-12.col-md-8.landing__features-section.row.text-left
-          .col-12.text-left.text-subtitle1.text-bold.q-mt-lg.q-mb-md.q-mx-xs Vue组件
-          router-link.landing__features-link.col-6.col-sm-4(
-            v-for="dir in features.comps"
-            :key="dir.name"
-            :to="'/vue-components/' + dir.path"
-          ) {{ dir.name }}
-
-        .col-6.col-sm-4.col-md-2.landing__features-section.column.text-left
-          .text-left.text-subtitle1.text-bold.q-mt-lg.q-mb-md.q-mx-xs 指令
-          router-link.landing__features-link(
-            v-for="dir in features.dirs"
-            :key="dir.name"
-            :to="'/vue-directives/' + dir.path"
-          ) {{ dir.name }}
-
-        .col-6.col-sm-4.col-md-2.landing__features-section.column.text-left
-          .text-left.text-subtitle1.text-bold.q-mt-lg.q-mb-md.q-mx-xs 插件
-          router-link.landing__features-link(
-            v-for="dir in features.plugins"
-            :key="dir.name"
-            :to="'/quasar-plugins/' + dir.path"
-          ) {{ dir.name }}
-
-  section.bg-white.text-grey-10.text-center.row.justify-center
-    .landing__features.row
-      .col-12.q-my-xl.row.items-center.justify-center
-        q-img.col-12.col-sm-4.landing__icongenie-logo(src="https://cdn.quasar.dev/img/iconfactory.png")
-        .col-12.col-sm-8.column.items-center
-          h4 Quasar Icon Genie
-          .q-mt-lg.q-mb-xl Consumes a source icon and automatically clones, scales, minifies and places the (needed) 100+ generated icons and splash screens in the appropriate directories for you.
-          q-btn(color="brand-primary", unelevated, no-caps, no-wrap, :icon-right="mdiLaunch", label="IconGenie", to="/icongenie/introduction")
-
-  section.landing__footer.flex.flex-center
-    div.text-center
-      div.landing__footer-icons.row.flex-center.q-gutter-md
-        a(href="https://github.quasar.dev", target="_blank", rel="noopener")
-          q-icon(:name="fabGithub")
-
-        a(href="https://blog.quasar.dev", target="_blank", rel="noopener")
-          q-icon(:name="mdiPost")
-
-        a(href="https://chat.quasar.dev", rel="noopener", target="_blank")
-          q-icon(:name="mdiChat")
-
-        a(href="https://forum.quasar.dev/", rel="noopener", target="_blank")
-          q-icon(:name="mdiForum")
-
-        a(href="https://twitter.quasar.dev", target="_blank", rel="noopener")
-          q-icon(:name="fabTwitter")
-
-        a(href="https://facebook.quasar.dev", target="_blank", rel="noopener")
-          q-icon(:name="fabFacebook")
-
-        a(href="https://donate.quasar.dev", rel="sponsored", target="_blank")
-          q-icon(:name="fasSuitcaseMedical")
-
-      div.q-mt-lg
-        | <doc-link to="https://github.com/quasarframework/quasar/blob/dev/LICENSE">MIT LICENSE</doc-link> | <doc-link to="https://www.iubenda.com/privacy-policy/40685560">隐私政策</doc-link> | <doc-link to="https://github.com/quasarframework/quasar-art">优秀Quasar作品集</doc-link>
-
-      div <doc-link to="http://beian.miit.gov.cn/">鄂ICP备2022014971号-1</doc-link>
+<template>
+  <q-page class="landing">
+    <h1 class="landing__title">Quasar Framework 中文网站</h1>
+    <section class="landing__hero flex flex-center">
+      <div class="row items-center justify-center landing__hero-inner">
+        <div class="landing__hero-logo col-12 flex flex-center q-mb-lg"><img class="landing__logo-image"
+            src="https://cdn.quasar.dev/logo-v2/svg/logo-vertical.svg"></div>
+        <div class="landing__hero-text col-12 column items-center">
+          <!--  .text-h4.landing__hero-text-main.text-bold.text-brand-primary.q-pb-xs QUASAR FRAMEWORK -->
+          <div class="q-pb-lg text-grey-6 landing__hero-quote text-center">帮助您在短时间内快速构建高性能，高质量的
+            <span class="text-bold text-no-wrap">Vue.js 3</span> 应用程序
+          </div>
+          <div class="landing__hero-row q-gutter-sm row items-center">
+            <q-btn color="brand-primary" unelevated no-caps to="/introduction-to-quasar" label="为何选择Quasar?"></q-btn>
+            <q-btn color="brand-primary" outline no-caps to="/start" label="Get Started"></q-btn>
+            <q-btn color="accent" unelevated no-caps to="/video-tutorials" label="视频教程"></q-btn>
+          </div>
+          <div class="q-pb-lg text-grey-6 landing__hero-quote text-center">本中文网站持续翻译中,信息来自：<a target="_blank"
+              class="text-bold text-no-wrap" href="https://quasar.dev">英文官网</a>。
+            如果想了解翻译进度，或者参与翻译，请访问 <a target="_blank" class="text-bold text-no-wrap"
+              href="https://github.com/dongwa/quasar-docs-cn">仓库地址</a>。
+            另外，我的B站账号中有 <a target="_blank" class="text-bold text-no-wrap"
+              href="https://www.bilibili.com/video/BV1pA4y197Zc">quasar 的视频教程</a>欢迎观看👏</div>
+        </div>
+      </div>
+    </section>
+    <!-- <section class="landing__front text-center">
+      <div>
+        <div class="row justify-center">
+          <introduction-video class="landing__video"></introduction-video>
+        </div>
+      </div>
+    </section> -->
+    <!-- <section class="bg-white text-grey-10 text-center">
+      <div>
+        <div class="text-h4 q-mb-xl">我们的高级赞助商</div>
+        <sponsor-list></sponsor-list>
+      </div>
+    </section> -->
+    <section class="bg-white text-grey-10 text-center row justify-center">
+      <div class="landing__features row">
+        <div class="col-12 text-center">
+          <h4>一套代码支持全平台</h4>
+        </div>
+        <div class="col-12 text-center q-my-xl">
+          <div class="landing__features-icons q-gutter-lg">
+            <q-icon :name="fabApple"></q-icon>
+            <q-icon :name="fabWindows"></q-icon>
+            <q-icon :name="fabLinux"></q-icon>
+            <q-icon :name="fabAndroid"></q-icon>
+            <q-icon :name="fabChrome"></q-icon>
+            <q-icon :name="fabFirefox"></q-icon>
+            <q-icon :name="fabEdge"></q-icon>
+            <q-icon :name="fabSafari"></q-icon>
+          </div>
+        </div>
+        <div class="col-12 text-center q-my-xl">
+          <p>使用官网脚手架 @quasar/cli 创建的 quasar 项目可以一套源代码编译成所有平台的应用程序，并提供最佳实践。您只需关注应用功能本身，忘记跨端带来的烦恼。</p>
+        </div>
+        <div class="col-12 row justify-center">
+          <q-card class="bg-grey-1 q-px-lg q-py-sm" bordered flat>
+            <q-card-section horizontal>
+              <q-card-section class="text-left column items-center q-gutter-sm">
+                <q-badge label="$ yarn global add @quasar/cli" color="grey-8"></q-badge>
+                <q-badge label="$ yarn create quasar" color="grey-8"></q-badge>
+              </q-card-section>
+              <q-separator vertical></q-separator>
+              <q-card-section class="text-left column items-center q-gutter-sm">
+                <q-badge label="$ npm i -g @quasar/cli" color="grey-8"></q-badge>
+                <q-badge label="$ npm init quasar" color="grey-8"></q-badge>
+              </q-card-section>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+    </section>
+    <section class="bg-white text-grey-10 text-center">
+      <div class="landing__features row items-start q-col-gutter-lg">
+        <div class="col-12 text-center">
+          <h4>令人形象深刻的用户界面组件</h4>
+        </div>
+        <div class="col-12 row">
+          <div class="col-12 col-md-8 landing__features-section row text-left">
+            <div class="col-12 text-left text-subtitle1 text-bold q-mt-lg q-mb-md q-mx-xs">Vue组件</div>
+            <router-link class="landing__features-link col-6 col-sm-4" v-for="dir in features.comps" :key="dir.name"
+              :to="'/vue-components/' + dir.path">{{ dir.name }}</router-link>
+          </div>
+          <div class="col-6 col-sm-4 col-md-2 landing__features-section column text-left">
+            <div class="text-left text-subtitle1 text-bold q-mt-lg q-mb-md q-mx-xs">指令</div>
+            <router-link class="landing__features-link" v-for="dir in features.dirs" :key="dir.name"
+              :to="'/vue-directives/' + dir.path">{{ dir.name }}</router-link>
+          </div>
+          <div class="col-6 col-sm-4 col-md-2 landing__features-section column text-left">
+            <div class="text-left text-subtitle1 text-bold q-mt-lg q-mb-md q-mx-xs">插件</div>
+            <router-link class="landing__features-link" v-for="dir in features.plugins" :key="dir.name"
+              :to="'/quasar-plugins/' + dir.path">{{ dir.name }}</router-link>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-white text-grey-10 text-center row justify-center">
+      <div class="landing__features row">
+        <div class="col-12 q-my-xl row items-center justify-center">
+          <q-img class="col-12 col-sm-4 landing__icongenie-logo" src="https://cdn.quasar.dev/img/iconfactory.png">
+          </q-img>
+          <div class="col-12 col-sm-8 column items-center">
+            <h4>Quasar Icon Genie</h4>
+            <div class="q-mt-lg q-mb-xl">
+              只需一个源图标，然后自动克隆、缩放、裁剪生成所需要的100+图标和启动画面，并将其放置在正确的目录下
+            </div>
+            <q-btn color="brand-primary" unelevated no-caps no-wrap :icon-right="mdiLaunch" label="IconGenie"
+              to="/icongenie/introduction"></q-btn>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="landing__footer flex flex-center">
+      <div class="text-center">
+        <div class="landing__footer-icons row flex-center q-gutter-md"><a href="https://github.quasar.dev"
+            target="_blank" rel="noopener">
+            <q-icon :name="fabGithub"></q-icon>
+          </a><a href="https://blog.quasar.dev" target="_blank" rel="noopener">
+            <q-icon :name="mdiPost"></q-icon>
+          </a><a href="https://chat.quasar.dev" rel="noopener" target="_blank">
+            <q-icon :name="mdiChat"></q-icon>
+          </a><a href="https://forum.quasar.dev/" rel="noopener" target="_blank">
+            <q-icon :name="mdiForum"></q-icon>
+          </a><a href="https://twitter.quasar.dev" target="_blank" rel="noopener">
+            <q-icon :name="fabTwitter"></q-icon>
+          </a><a href="https://facebook.quasar.dev" target="_blank" rel="noopener">
+            <q-icon :name="fabFacebook"></q-icon>
+          </a><a href="https://donate.quasar.dev" rel="sponsored" target="_blank">
+            <q-icon :name="fasSuitcaseMedical"></q-icon>
+          </a></div>
+        <div class="q-mt-lg">
+          <doc-link to="https://github.com/quasarframework/quasar/blob/dev/LICENSE">MIT LICENSE</doc-link> | <doc-link
+            to="https://www.iubenda.com/privacy-policy/40685560">隐私政策</doc-link> | <doc-link
+            to="https://github.com/quasarframework/quasar-art">优秀Quasar作品集</doc-link>
+        </div>
+        <div>
+          <doc-link to="http://beian.miit.gov.cn/">鄂ICP备2022014971号-1</doc-link>
+        </div>
+      </div>
+    </section>
+  </q-page>
 </template>
 
 <script>
 import { useMeta } from 'quasar'
 
-import Sponsor from 'components/page-parts/sponsors-and-backers/Sponsor.vue'
-import SponsorList from 'components/page-parts/sponsors-and-backers/SponsorList.vue'
-import IntroductionVideo from 'components/page-parts/introduction-to-quasar/IntroductionVideo.vue'
+// import Sponsor from 'components/page-parts/sponsors-and-backers/Sponsor.vue'
+// import SponsorList from 'components/page-parts/sponsors-and-backers/SponsorList.vue'
+// import IntroductionVideo from 'components/page-parts/introduction-to-quasar/IntroductionVideo.vue'
 // import SurveyCountdown from '../components/SurveyCountdown.vue'
 
 import {
@@ -162,10 +184,10 @@ export default {
   name: 'Landing',
 
   components: {
-    Sponsor,
-    SponsorList,
+    // Sponsor,
+    // SponsorList,
     // SurveyCountdown,
-    IntroductionVideo
+    // IntroductionVideo
   },
 
   setup () {
