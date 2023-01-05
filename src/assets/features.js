ac52570d2ff935265f9519b1af669ad0dbb43a49
dongwa <783137663@qq.com>
2023-01-05 17:43:55 +0800

    fix: 修复首页用户接口区域链接错误问题

--- src/assets/features.js
+++ src/assets/features.js
@@ -48,6 +48,6 @@ function normalizeComps (list) {
 
 export default {
   comps: normalizeComps(menu.find(entry => entry.name === 'Vue 组件').children),
-  dirs: menu.find(entry => entry.name === 'Vue 指令').children,
-  plugins: menu.find(entry => entry.name === 'Quasar 插件').children
+  dirs: menu.find(entry => entry.name === 'Vue 指令').children.map(entry => ({ ...entry, path: `/vue-directives/${entry.path}` })),
+  plugins: menu.find(entry => entry.name === 'Quasar 插件').children.map(entry => ({ ...entry, path: `/quasar-plugins/${entry.path}` }))
 }
